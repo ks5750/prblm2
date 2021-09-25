3985907e123fc224f793d1e5ca040701f5b43141
kumar <kumar.saswat@gmail.com>
2021-09-24 22:40:11 -0400

    Problem 4

--- solution.py
+++ solution.py
@@ -11,9 +11,9 @@ import secrets
 import os
 
 
-with open(sys.argv[1]) as json_data:
-     inputs = json.load(json_data)
-#inputs = json.load(sys.stdin)
+#with open(sys.argv[1]) as json_data:
+#     inputs = json.load(json_data)
+inputs = json.load(sys.stdin)
 
 outputs = {}
 def xor_bytes(a, b):
